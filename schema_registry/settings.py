2f0295a60481b2452ec614a5d3f5cfaa78fa4dc4
Dmitry Kolyagin <dmitry.kolyagin@gmail.com>
2021-12-19 19:58:54 +0100

    Read env variables from .env file

--- schema_registry/settings.py
+++ schema_registry/settings.py
@@ -9,6 +9,9 @@ environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Read env variables from .env file
+env_file = BASE_DIR / '.env'
+environ.Env.read_env(env_file=env_file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
